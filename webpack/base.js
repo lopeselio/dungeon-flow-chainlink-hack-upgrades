7f9bf978dfdd1ec423ca4cf82f3fac4c70372d03
devELIOper <lopeselio@gmail.com>
2023-06-09 17:51:31 -0700

    webpack config

--- webpack/base.js
+++ webpack/base.js
@@ -16,7 +16,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 //         "https": false,
 //         "stream": false,
 //         "crypto": false,
-//         "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
+//         "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify. 
 //     }
 // }
 
