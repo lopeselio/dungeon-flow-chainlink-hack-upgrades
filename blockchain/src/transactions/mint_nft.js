844d6834198d6436db334becf02dd6fbbd667c80
devELIOper <lopeselio@gmail.com>
2023-06-09 17:37:25 -0700

    Create mint_nft.js

---
+++ blockchain/src/transactions/mint_nft.js
@@ -0,0 +1,19 @@
+export const mintNFT = `
+import MyNFT from 0x9f690718478ff417
+
+transaction(ipfsHash: String, name: String) {
+
+  prepare(acct: AuthAccount) {
+    let collection = acct.borrow<&MyNFT.Collection>(from: /storage/MyNFTCollection)
+                        ?? panic("This collection does not exist here")
+
+    let nft <- MyNFT.createToken(ipfsHash: ipfsHash, metadata: {"name": name})
+
+    collection.deposit(token: <- nft)
+  }
+
+  execute {
+    log("A user minted an NFT into their account")
+  }
+}
+`
